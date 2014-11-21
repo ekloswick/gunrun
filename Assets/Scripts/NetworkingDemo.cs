9a5ce752277bbc2e54c36011e1cb1c6782a30f93
Eli <ekloswic@alumni.nd.edu>
2014-11-20 21:49:45 -0600

    simple server initialization/connection

---
+++ Assets/Scripts/NetworkingDemo.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NetworkingDemo : MonoBehaviour {
+
+    public Text ipText;
+    public Text portText;
+
+    private string ipAddress = "";
+    private int port = 0;
+
+    void Start()
+    {
+        Invoke("printDebug", 0.4f);
+    }
+
+    public void hostServer()
+    {
+        Network.InitializeServer(4, port, true);
+        Debug.Log("Hosting on 127.0.0.1, listening on port " + port);
+    }
+
+    public void connectToServer()
+    {
+        ipAddress = ipText.text;
+        int.TryParse(portText.text, out port);
+
+        if (!string.IsNullOrEmpty(ipAddress) && !string.IsNullOrEmpty(port.ToString()))
+        {
+            Network.Connect(ipAddress, port);
+        }
+        else
+        {
+            Debug.Log("Connect error: invalid IP address or port");
+        }
+    }
+
+    public void printDebug()
+    {
+        Debug.Log(ipAddress + ':' + port);
+        Invoke("printDebug", 0.4f);
+    }
+}

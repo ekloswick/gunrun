c45319f72c572abcbd0b6d70d25a35250403a473
Eli <ekloswic@alumni.nd.edu>
2014-11-20 22:39:13 -0600

    finished simple networking demo

--- Assets/Scripts/NetworkingDemo.cs
+++ Assets/Scripts/NetworkingDemo.cs
@@ -6,39 +6,111 @@ public class NetworkingDemo : MonoBehaviour {
 
     public Text ipText;
     public Text portText;
+    public Text connectionsText;
+
+    public Button disconnectButton;
 
     private string ipAddress = "";
     private int port = 0;
+    private bool connected = false;
 
-    void Start()
+    void Update()
     {
-        Invoke("printDebug", 0.4f);
+        if (connected)
+        {
+            connectionsText.text = "Connections: " + Network.connections.Length.ToString();
+            disconnectButton.interactable = true;
+        }
+        else
+        {
+            connectionsText.text = "Not connected";
+            disconnectButton.interactable = false;
+        }
     }
 
     public void hostServer()
     {
-        Network.InitializeServer(4, port, true);
-        Debug.Log("Hosting on 127.0.0.1, listening on port " + port);
+        if (!connected)
+        {
+            if (!string.IsNullOrEmpty(portText.text))
+            {
+                int.TryParse(portText.text, out port);
+                Network.InitializeServer(4, port, true);
+                Debug.Log("Listening on port " + port);
+            }
+            else
+            {
+                Debug.Log("Invalid port number");
+            }
+
+        }
+        else
+        {
+            Debug.Log("Already connected to a server");
+        }
     }
 
     public void connectToServer()
     {
-        ipAddress = ipText.text;
-        int.TryParse(portText.text, out port);
-
-        if (!string.IsNullOrEmpty(ipAddress) && !string.IsNullOrEmpty(port.ToString()))
+        if (!connected)
         {
-            Network.Connect(ipAddress, port);
+            ipAddress = ipText.text;
+            int.TryParse(portText.text, out port);
+
+            // try to connect
+            if (!string.IsNullOrEmpty(ipAddress) && !string.IsNullOrEmpty(port.ToString()))
+            {
+                Network.Connect(ipAddress, port);
+            }
+            else
+            {
+                Debug.Log("Connect error: invalid IP address or port");
+            }
         }
         else
         {
-            Debug.Log("Connect error: invalid IP address or port");
+            Debug.Log("Already connected to a server");
+        }
+        
+    }
+
+    void OnServerInitialized()
+    {
+        Debug.Log("Hosting on 127.0.0.1, listening on port " + port);
+        connected = true;
+    }
+
+    void OnConnectedToServer()
+    {
+        Debug.Log("Connected to " + ipAddress + ":" + port);
+        connected = true;
+    }
+
+    void OnPlayerConnected()
+    {
+        Debug.Log("Player has connected");
+    }
+
+    void OnPlayerDisconnected()
+    {
+        Debug.Log("Player has disconnected");
+    }
+
+    public void disconnectFromServer()
+    {
+        Network.Disconnect();
+        if (Network.isServer)
+        {
+            Debug.Log("Server successfully closed");
+        }
+        else
+        {
+            Debug.Log("Disconnected from server");
         }
     }
 
-    public void printDebug()
+    void OnDisconnectedFromServer()
     {
-        Debug.Log(ipAddress + ':' + port);
-        Invoke("printDebug", 0.4f);
+        connected = false;
     }
 }

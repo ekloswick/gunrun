397851e128d265781d12ffafa56d65db6da39b67
ian-p-mcpherson <ian.p.mcpherson@gmail.com>
2015-01-25 17:57:17 -0500

    Added gunScript and UIManager scripts for UI and gunplay testing. Added sounds for temporary testing.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	public GameObject AmmoDisplay;
+	public GameObject HitpointDisplay;
+
+	private Text ammoText;
+	private Image bulletDisplay;
+
+	// Use this for initialization
+	void Start () {
+		ammoText = AmmoDisplay.GetComponent<Text> ();
+		bulletDisplay = AmmoDisplay.GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void updateAmmo(int ammoCount)
+	{
+		ammoText.text = ammoCount.ToString();
+		bulletDisplay.rectTransform.sizeDelta = new Vector2(ammoCount * 16, 64);
+	}
+
+	public void reloadAnimate()
+	{
+		// Nothing as of now
+	}
+}
---
+++ Assets/Scripts/gunScript.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gunScript : MonoBehaviour {
+	
+	public int maxAmmo = 16;
+	public int currentAmmo = 16;
+	public float fireRate = 0.1f;
+	public float reloadSpeed = 3.0f;
+
+	public GameObject UICanvas;
+	public UIManager CanvasUIManager;
+
+	public bool reloading = false;
+	public bool firing = false;
+
+	public AudioClip gunshotSound;
+	public AudioClip reloadSound;
+	public AudioClip dryfireSound;
+
+	// Use this for initialization
+	void Start () {
+		UICanvas = GameObject.FindGameObjectWithTag ("UIManager");
+		CanvasUIManager = UICanvas.GetComponent<UIManager>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButton (0))
+		{
+			if(!(firing || reloading))
+			{
+				fire ();
+			}
+		} 
+
+		if (Input.GetKey ("r"))
+		{
+			if(!(firing || reloading))
+			{
+				reload ();
+			}
+		}
+	}
+
+	void fire() {
+		if(currentAmmo > 0)
+		{
+			firing = true;
+			shootBullet();
+			AudioSource.PlayClipAtPoint(gunshotSound, transform.position);
+			currentAmmo--;
+			//CanvasUIManager.GetComponent<UIManager>().updateAmmo(currentAmmo);
+			StartCoroutine(fireCooldown());
+		}
+		else
+		{
+			firing = true;
+			AudioSource.PlayClipAtPoint(dryfireSound, transform.position);
+			StartCoroutine(fireCooldown());
+		}
+	}
+
+	void shootBullet()
+	{
+		// Do bullet things
+	}
+
+	IEnumerator fireCooldown()
+	{
+		yield return new WaitForSeconds(fireRate);
+		firing = false;
+	}
+
+	void reload() {
+		reloading = true;
+		//CanvasUIManager.GetComponent<UIManager>().reloadAnimate();
+		AudioSource.PlayClipAtPoint(reloadSound, transform.position);
+		StartCoroutine(reloadCooldown());
+	}
+
+	IEnumerator reloadCooldown()
+	{
+		yield return new WaitForSeconds(reloadSpeed);
+		currentAmmo = maxAmmo;
+		//CanvasUIManager.GetComponent<UIManager>().updateAmmo(currentAmmo);
+		reloading = false;
+	}
+
+}
